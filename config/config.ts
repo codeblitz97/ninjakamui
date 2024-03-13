13ea0344d21d5c69812b10244a6a1bfb1afbaecc
Mohtasim Alam Sohom <94981761+codeblitz97@users.noreply.github.com>
2024-03-13 23:51:34 +0600

    Update server and app configuration

---
+++ config/config.ts
@@ -0,0 +1,3 @@
+export const consumetApi = 'http://69.57.161.113:29932/meta/anilist';
+export const anifyApi = 'https://api.anify.tv';
+export const streamApi = 'http://69.57.161.113:29932/anime';
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: 's4.anilist.co',
+        port: '',
+        protocol: 'https',
+        pathname: '/**',
+      },
+      {
+        hostname: 'i.ytimg.com',
+        port: '',
+        protocol: 'https',
+        pathname: '/**',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
