37ef0bdad7236b3127a483dd9f361e19ee193750
Mohtasim Alam Sohom <94981761+codeblitz97@users.noreply.github.com>
2024-03-18 16:38:47 +0600

    Update API URI

--- config/config.ts
+++ config/config.ts
@@ -1,3 +1,3 @@
-export const consumetApi = 'http://69.57.161.113:29932/meta/anilist';
+export const consumetApi = `${process.env.NINJA_KAMUI_API}/meta/anilist`;
 export const anifyApi = 'https://api.anify.tv';
-export const streamApi = 'http://69.57.161.113:29932/anime';
+export const streamApi = `${process.env.NINJA_KAMUI_API}/anime`;
